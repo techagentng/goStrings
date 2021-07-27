47d5102ac306f62cc7a407b90485592344aa9a8f
Capabay <capabay@hotmail.com>
2021-07-27 22:12:19 +0100

    last commit

--- hello.go
+++ hello.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var name string = "Nnamdi"
+	var name string = "Nnamdi,"
 	var stack string = "Golang"
 	var squad string = "SQ008"
-	var year int = 2021
-	fmt.Println("my Name is", name, "I am a ", stack, "developer","a member of ", squad, "of year", year)
+	var year int = 2021.
+	fmt.Println("My Name is", name, "I am a",stack, "developer,","a member of",squad, "of year", year)
 }
