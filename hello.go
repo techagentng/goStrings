fa82cf8fdb5266617cd808c2ff3e41db96b89917
kenneth <techagentng@gmail.com>
2021-07-27 07:57:31 +0100

    commiting test go code

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Nnamdi"
+	var stack string = "Golang"
+	var squad string = "SQ008"
+	var year int = 2021
+	fmt.Println("my Name is", name, "I am a ", stack, "developer","a member of ", squad, "of year", year)
+}
